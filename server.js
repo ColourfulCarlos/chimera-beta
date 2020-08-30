7da7dce46ce99b0bd4ad04bd5c01a5f8dcf58b28
ColourfulCarlos <cgoncalves1991@hotmail.com>
2020-08-30 21:51:41 +0100

    unnistalled nodejs and express

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public'))); 
-var port = 8000; // you can use any port
-app.listen(port);
-console.log('server on' + port);
